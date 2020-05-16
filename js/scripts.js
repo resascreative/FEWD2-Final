59043b0a3e8ad4a46b658d60c19eb8422420490f
resascreative <tsmithgd@gmail.com>
2020-05-15 20:53:59 -0700

    --- Auto-Git Commit ---

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+var DoughType = {
+    HT : "Hand Tossed",
+    TC : "Thin Crust",
+    NYS : "New York Style",
+    GF : "Gluten Free"
+};
+
+var gSizes = {
+    "Hand Tossed": [
+        {
+            size: "Small",
+            cost: 9.99
+        },
+        {
+            size: "Medium",
+            cost: 12.99,
+            check: true
+        },
+        {
+            size: "Large",
+            cost: 14.99
+        }
+    ],
+    "Thin Crust": [
+        {
+            size: "Medium",
+            cost: 11.99,
+            check: true
+        },
+        {
+            size: "Large",
+            cost: 13.99
+        }
+    ],
+    "New York Style": [
+        {
+            size: "Large",
+            cost: 14.99,
+            check: true
+        },
+        {
+            size: "Extra Large",
+            cost: 19.99
+        }
+    ],
+    "Gluten Free": [
+        {
+            size: "Small",
+            cost: 10.99,
+            check: true
+        }
+    ]
+};
