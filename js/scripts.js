40633889dd6c79085562d99a3f4870df610116b3
resascreative <tsmithgd@gmail.com>
2020-05-16 13:27:34 -0700

    --- Auto-Git Commit ---

--- js/scripts.js
+++ js/scripts.js
@@ -11,56 +11,56 @@
     }, false);
 
 
-let crustType = {
-    HT : "Hand Tossed",
-    TC : "Thin Crust",
-    NYS : "New York Style",
-    GF : "Gluten Free"
-};
+// let crustType = {
+//     HT : "Hand Tossed",
+//     TC : "Thin Crust",
+//     NYS : "New York Style",
+//     GF : "Gluten Free"
+// };
 
-let sizeCost = {
-    "Hand Tossed": [
-        {
-            size: "Small",
-            cost: 9.99
-        },
-        {
-            size: "Medium",
-            cost: 12.99,
-            // check: true
-        },
-        {
-            size: "Large",
-            cost: 14.99
-        }
-    ],
-    "Thin Crust": [
-        {
-            size: "Medium",
-            cost: 11.99,
-            check: true
-        },
-        {
-            size: "Large",
-            cost: 13.99
-        }
-    ],
-    "New York Style": [
-        {
-            size: "Large",
-            cost: 14.99,
-            check: true
-        },
-        {
-            size: "Extra Large",
-            cost: 19.99
-        }
-    ],
-    "Gluten Free": [
-        {
-            size: "Small",
-            cost: 10.99,
-            check: true
-        }
-    ]
-};
+// let sizeCost = {
+//     "Hand Tossed": [
+//         {
+//             size: "Small",
+//             cost: 9.99
+//         },
+//         {
+//             size: "Medium",
+//             cost: 12.99,
+//             // check: true
+//         },
+//         {
+//             size: "Large",
+//             cost: 14.99
+//         }
+//     ],
+//     "Thin Crust": [
+//         {
+//             size: "Medium",
+//             cost: 11.99,
+//             check: true
+//         },
+//         {
+//             size: "Large",
+//             cost: 13.99
+//         }
+//     ],
+//     "New York Style": [
+//         {
+//             size: "Large",
+//             cost: 14.99,
+//             check: true
+//         },
+//         {
+//             size: "Extra Large",
+//             cost: 19.99
+//         }
+//     ],
+//     "Gluten Free": [
+//         {
+//             size: "Small",
+//             cost: 10.99,
+//             check: true
+//         }
+//     ]
+// };
