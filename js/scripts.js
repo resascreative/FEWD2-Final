212651b2c3d2e76e4dd58aeea59d183f2cef6d57
resascreative <tsmithgd@gmail.com>
2020-05-17 14:14:40 -0700

    --- Auto-Git Commit ---

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 // OTHER ADDRESS TYPE 
-    let extraAddType = document.getElementById("addressType2");
+    let AddType = document.getElementById("addressType");
     let otherAdd = document.getElementById("otherAddType");//hidden other address text field 
 
-    extraAddType.addEventListener("change", function(){
-        if (extraAddType.value = "other"){
+    AddType.addEventListener("change", function(){
+        if (AddType.value = "other"){
             otherAdd.setAttribute("class", "show");
         }else {
             otherAdd.setAttribute("class", "hidden");
@@ -14,56 +14,56 @@
 
 
 
-// let crustType = {
-//     HT : "Hand Tossed",
-//     TC : "Thin Crust",
-//     NYS : "New York Style",
-//     GF : "Gluten Free"
-// };
+let crustType = {
+    HT : "Hand Tossed",
+    TC : "Thin Crust",
+    NYS : "New York Style",
+    GF : "Gluten Free"
+};
 
-// let sizeCost = {
-//     "Hand Tossed": [
-//         {
-//             size: "Small",
-//             cost: 9.99
-//         },
-//         {
-//             size: "Medium",
-//             cost: 12.99,
-//             // check: true
-//         },
-//         {
-//             size: "Large",
-//             cost: 14.99
-//         }
-//     ],
-//     "Thin Crust": [
-//         {
-//             size: "Medium",
-//             cost: 11.99,
-//             check: true
-//         },
-//         {
-//             size: "Large",
-//             cost: 13.99
-//         }
-//     ],
-//     "New York Style": [
-//         {
-//             size: "Large",
-//             cost: 14.99,
-//             check: true
-//         },
-//         {
-//             size: "Extra Large",
-//             cost: 19.99
-//         }
-//     ],
-//     "Gluten Free": [
-//         {
-//             size: "Small",
-//             cost: 10.99,
-//             check: true
-//         }
-//     ]
-// };
+let sizeCost = {
+    "HandTossed": [
+        {
+            size: "Small",
+            cost: 9.99
+        },
+        {
+            size: "Medium",
+            cost: 12.99,
+            // check: true
+        },
+        {
+            size: "Large",
+            cost: 14.99
+        }
+    ],
+    "ThinCrust": [
+        {
+            size: "Medium",
+            cost: 11.99,
+            check: true
+        },
+        {
+            size: "Large",
+            cost: 13.99
+        }
+    ],
+    "NewYorkStyle": [
+        {
+            size: "Large",
+            cost: 14.99,
+            check: true
+        },
+        {
+            size: "Extra Large",
+            cost: 19.99
+        }
+    ],
+    "GlutenFree": [
+        {
+            size: "Small",
+            cost: 10.99,
+            check: true
+        }
+    ]
+};
