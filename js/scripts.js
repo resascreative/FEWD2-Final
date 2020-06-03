78782ff096e60c863d6ca6cc4b4ef4050a27d7bf
resascreative <tsmithgd@gmail.com>
2020-06-02 22:53:41 -0700

    --- Auto-Git Commit ---

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@
     }, false);
 
     document.getElementById("addressType").addEventListener("show", "otherAddType");
-    
+
 //hide and show crust drop downs
 $('.dropHandToss').hide();
 $('.dropThinCrust').hide();
@@ -42,24 +42,34 @@ $('#customRadio4').click(() => {
     $('.dropNY').hide();
     $('.dropGFree').show();
 })
+
 // SAME AS DELIVERY
-function FillBilling(f) {
-    if(f.sameinfo.checked == true) {
-      f.inputName2.value = f.inputName.value;
-      f.inputAddress2.value = f.inputAddress.value;
-      f.inputAddress01.value = f.inputAddress0.value;
-      f.inputCity2.value = f.inputCity.value;
-      f.inputState2.value = f.inputState.value;
-      f.inputZip2.value = f.inputZip.value;
-    }
-  }
+$('#sameinfo').click(()=> {
+    "use strict";
+    $('#inputName2').val($('#inputName').val());
+    $('#inputAddress2').val($('#inputAddress').val());
+    $('#inputAddress01').val($('#inputAddress0').val());
+    $('#inputCity2').val($('#inputCity').val());
+    $('#inputState2').val($('#inputState').val());
+    $('#inputZip2').val($('#inputZip').val());
+});
+// function FillBilling(f) {
+//     if(f.sameinfo.checked == true) {
+//       f.inputName2.value = f.inputName.value;
+//       f.inputAddress2.value = f.inputAddress.value;
+//       f.inputAddress01.value = f.inputAddress0.value;
+//       f.inputCity2.value = f.inputCity.value;
+//       f.inputState2.value = f.inputState.value;
+//       f.inputZip2.value = f.inputZip.value;
+//     }
+//   }
 
    //REGEX
    let validation = {
     name: /^(?![\s.]+$)[a-zA-Z\s.]*$/,
-    address: /^\d+[ ](?:[A-Za-z0-9.-]+[ ]?)+(?:Avenue|Lane|Road|Boulevard|Drive|Street|Ave|Dr|Rd|Blvd|Ln|St)\.?/,
-    citytype:/^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/,
-    statetype:/^([Aa][LKSZRAEPlkszraep]|[Cc][AOTaot]|[Dd][ECec]|[Ff][LMlm]|[Gg][AUau]|[Hh][Ii]|[Ii][ADLNadln]|[Kk][SYsy]|[Ll][Aa]|[Mm][ADEHINOPSTadehinopst]|[Nn][CDEHJMVYcdehjmvy]|[Oo][HKRhkr]|[Pp][ARWarw]|[Rr][Ii]|[Ss][CDcd]|[Tt][NXnx]|[Uu][Tt]|[Vv][AITait]|[Ww][AIVYaivy])$/,
+    address: /^\d+[ ](?:[A-Za-z0-9.-]+[ ]?)+(?:Avenue|Drive|Road|Boulevard|Lane|Street|Ave|Dr|Rd|Blvd|Ln|St)\.?/,
+    city:/^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/,
+    state:/^([Aa][LKSZRAEPlkszraep]|[Cc][AOTaot]|[Dd][ECec]|[Ff][LMlm]|[Gg][AUau]|[Hh][Ii]|[Ii][ADLNadln]|[Kk][SYsy]|[Ll][Aa]|[Mm][ADEHINOPSTadehinopst]|[Nn][CDEHJMVYcdehjmvy]|[Oo][HKRhkr]|[Pp][ARWarw]|[Rr][Ii]|[Ss][CDcd]|[Tt][NXnx]|[Uu][Tt]|[Vv][AITait]|[Ww][AIVYaivy])$/,
     zip: /(^\d{5}$)|(^\d{9}$)|(^\d{5}-\d{4}$)/,
     email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
     phone:  /^\D?([2-9]{1})(\d{2})\D?\D?(\d{3})\D?(\d{4})$/,
@@ -69,60 +79,5 @@ function FillBilling(f) {
     amex: /^3[47][0-9]{13}$/,
     ccn:  /^(?:4[0-9]{12}(?:[0-9]{3})?|(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}|3[47][0-9]{13})$/,
 };
-
-// let crustType = {
-//     HT : "Hand Tossed",
-//     TC : "Thin Crust",
-//     NY : "New York Style",
-//     GF : "Gluten Free"
-// };
-
-// let sizeCost = {
-//     "Hand Tossed": [
-//         {
-//             size: "Small",
-//             cost: 9.99
-//         },
-//         {
-//             size: "Medium",
-//             cost: 12.99,
-//             // check: true
-//         },
-//         {
-//             size: "Large",
-//             cost: 14.99
-//         }
-//     ],
-//     "Thin Crust": [
-//         {
-//             size: "Medium",
-//             cost: 11.99,
-//             check: true
-//         },
-//         {
-//             size: "Large",
-//             cost: 13.99
-//         }
-//     ],
-//     "New York Style": [
-//         {
-//             size: "Large",
-//             cost: 14.99,
-//             check: true
-//         },
-//         {
-//             size: "Extra Large",
-//             cost: 19.99
-//         }
-//     ],
-//     "Gluten Free": [
-//         {
-//             size: "Small",
-//             cost: 10.99,
-//             check: true
-//         }
-//     ]
-// };
-
 
     
